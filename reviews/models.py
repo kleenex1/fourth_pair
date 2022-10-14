46fbb9c73bb0c458e6a00b31d25290456d39b2bb
rhoeunbin <rhoeb21@gmail.com>
2022-10-14 15:24:22 +0900

    django_review완료

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    movie_name = models.CharField(max_length=30)
+    RATING = [  (1, '★'),
+                (2, '★★'),
+                (3, '★★★'),
+                (4, '★★★★'),
+                (5, '★★★★★'),]
+    grade = models.IntegerField(choices=RATING, default=None)
+    created_at = models.DateTimeField(auto_now_add= True)
+    updated_at = models.DateTimeField(auto_now=True)
